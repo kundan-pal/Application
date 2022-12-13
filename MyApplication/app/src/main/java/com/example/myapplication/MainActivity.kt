d39a2575ed65b8118f0e6abbb22a7785b3d4b4e2
Kundan Pal <86739874+kundan-pal@users.noreply.github.com>
2022-12-13 13:52:21 +0530

    Commented the code
    Commented the code

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // getting camera_Button access by its id
+        // fetch camera_Button access by its id
         val clickCamera = findViewById<Button>(R.id.camera_Button)
         // checking if camera permission is granted or not if not the we will ask for the permission at the start of the app
         if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
@@ -42,8 +42,7 @@ class MainActivity : AppCompatActivity() {
 
         // when the camera button is clicked
         clickCamera.setOnClickListener {
-//            Toast.makeText(this, "Camera button is clicked", Toast.LENGTH_SHORT).show()
-            // again checking if the camera permission is given or not
+            // again checking if the camera permission is given or not in case user denies at the start
             if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
                 ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), REQUEST_CODE)
             }
@@ -51,53 +50,39 @@ class MainActivity : AppCompatActivity() {
                 // if the camera permission is given then start the camera for taking the picture
                 val takeIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
                 startActivityForResult(takeIntent, REQUEST_CODE)
-//                if(takeIntent.resolveActivity(packageManager)!=null){
-//                    startActivityForResult(takeIntent, REQUEST_CODE)
-//                }
             }
         }
-
-//        println("function call to fetchJson")
-//        fetchJson()
     }
 
     private fun fetchJson(){
         println("attempting to fetch json")
-        Log.d("this", "attempting to fetch JSON")
 
-
-//        val url = "https://api.letsbuildthatapp.com/youtube/home_feed"
-
+        // a temporary url for testing if our okhttp client is able to send request and fetch the data
 //        val url = "https://jsonplaceholder.typicode.com/todos/1"
 
-//        val url = "http://127.0.0.1:5000/returnjson"
-
-//        val url = "http://10.0.2.2:8080"
-
-        val url = "https://7832-104-28-193-172.in.ngrok.io/"
+        // declaring url variable which contains the url where we will send the post request.
+        val url = "https://59ea-104-28-219-119.in.ngrok.io"
 
 
+        // fetching the image taken from the imageview
         val imageView = findViewById<ImageView>(R.id.imageView)
         println("extracted imageview")
         val bitmap = (imageView.drawable as BitmapDrawable).bitmap
-
         println("created bitmap")
         val byteArrayOutputStream = ByteArrayOutputStream()
-//        val bitmap = BitmapFactory.decodeResource(/* res = */ resources, /* id = */ R.drawable.ic_launcher_background)
-
-//        println("created bitmap")
         bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream)
         println("created bitmap compress")
         val imageBytes: ByteArray = byteArrayOutputStream.toByteArray()
         println("created imageBytes")
         val postBodyText: String = Base64.encodeToString(imageBytes, Base64.DEFAULT)
-        println("created string")
+        println("created Base64 image string")
+
 //        println(postBodyText)
 //        val postBodyText = "Hello"
-
 //        textView.text = imageString
-
 //        val mediaType = MediaType.parse("text/plain; charset=utf-8")
+
+        // declaring post body
         val mediaType = "text/plain; charset=utf-8".toMediaTypeOrNull()
         val postBody: RequestBody = RequestBody.create(mediaType, postBodyText)
         val request = Request.Builder()
@@ -105,17 +90,17 @@ class MainActivity : AppCompatActivity() {
             .post(postBody)
             .build()
 
+
         val client = OkHttpClient()
 
         // we can not execute http request in the main thread so we need to use enqueue
-//        client.newCall(request).execute()
 
         var body = "$"
         val textView = findViewById<TextView>(R.id.textView)
-        println("Begin of functions")
+        println("Begin of http request")
         client.newCall(request).enqueue(object : Callback {
             override fun onResponse(call: Call, response: Response) {
-                println("trying to fetch json from the url")
+                println("trying to fetch json object string from the url")
                 body = response.body?.string().toString()
                 textView.text = body
                 println(body)
@@ -125,14 +110,7 @@ class MainActivity : AppCompatActivity() {
                 println("Failed to execute request")
             }
         })
-//        if()
-//
-//        if(body != "$"){
-//            textView.text = "hello world"
-//        }
-
-
-        println("End of the functions")
+        println("End of the function")
     }
 
     // optional function to make camera button enable if the permission is granted
@@ -149,16 +127,16 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    // after image is taken we display it in the imageview variable
+    // after image is taken we display it in the imageview variable and make http request
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if(requestCode == REQUEST_CODE && resultCode == RESULT_OK){  //means user had successfully taken picture from camera
+        if(requestCode == REQUEST_CODE && resultCode == RESULT_OK){     //means user had successfully taken picture from camera
             val imageTaken = data?.extras?.get("data") as Bitmap        // image we take comes as bitmap
             val imageView = findViewById<ImageView>(R.id.imageView)
             imageView.setImageBitmap(imageTaken)
 
-            println("function call to fetchJson")
+            println("Calling FetchJson function _____")
             fetchJson()
-            println("fetch json ended")
+            println("fetchJson function ended _____")
         }
         else{
             Toast.makeText(this, "Image is not taken", Toast.LENGTH_SHORT).show()
--- server/app.py
+++ server/app.py
@@ -3,28 +3,35 @@ import base64
 from PIL import Image
 import os
 
+# declaring a dictionary for objects corresponding to their indices
 labels = {0: 'person', 1: 'bicycle', 2: 'car', 3: 'motorcycle', 4: 'airplane', 5: 'bus', 6: 'train', 7: 'truck', 8: 'boat', 9: 'traffic', 10: 'fire', 11: 'stop', 12: 'parking', 13: 'bench', 14: 'bird', 15: 'cat', 16: 'dog', 17: 'horse', 18: 'sheep', 19: 'cow', 20: 'elephant', 21: 'bear', 22: 'zebra', 23: 'giraffe', 24: 'backpack', 25: 'umbrella', 26: 'handbag', 27: 'tie', 28: 'suitcase', 29: 'frisbee', 30: 'skis', 31: 'snowboard', 32: 'sports', 33: 'kite', 34: 'baseball', 35: 'baseball', 36: 'skateboard', 37: 'surfboard', 38: 'tennis', 39: 'bottle', 40: 'wine', 41: 'cup', 42: 'fork', 43: 'knife', 44: 'spoon', 45: 'bowl', 46: 'banana', 47: 'apple', 48: 'sandwich', 49: 'orange', 50: 'broccoli', 51: 'carrot', 52: 'hot', 53: 'pizza', 54: 'donut', 55: 'cake', 56: 'chair', 57: 'couch', 58: 'potted', 59: 'bed', 60: 'dining', 61: 'toilet', 62: 'tv', 63: 'laptop', 64: 'mouse', 65: 'remote', 66: 'keyboard', 67: 'cell', 68: 'microwave', 69: 'oven', 70: 'toaster', 71: 'sink', 72: 'refrigerator', 73: 'book', 74: 'clock', 75: 'vase', 76: 'scissors', 77: 'teddy', 78: 'hair', 79: 'toothbrush'}
 app = Flask(__name__)
 
+# declaring type of request this server is supposed to handle : get and post
 @app.route('/', methods=['GET', 'POST'])
 def handle_request():
+    # get request for fetching data from server
     if request.method == 'GET':
-        return jsonify("Flask Server & Android are Working Successfully !!!")
+        return jsonify("Flask Server is Running Successfully !!!")
+
+    # post request for sending data to the server and getting back response
     if request.method == 'POST':
         data =  request.data
         print(len(data))
-        image = base64.b64decode(data)
+        image = base64.b64decode(data)      # converting base64 string to image
         filename = "yolov5-master/image.jpg"
-        with open(filename, 'wb') as f:
+        with open(filename, 'wb') as f:     # storing image file into the server
             f.write(image)
         
-        image = Image.open('yolov5-master/image.jpg')
+        image = Image.open('yolov5-master/image.jpg')       # opening image for confimation that image capture by phones camera has been delivered to server
 
+        # executing terminal command using os library for yolo model processing on the image taken and storin results 
         os.system("cd yolov5-master && python detect.py --weights yolov5s.pt --img 640 --conf 0.25 --source image.jpg --save-txt --save-conf")
         
+        # declaring the path where results are being stored
         dir_list = os.listdir("yolov5-master/runs/detect")
         l2 = sorted(dir_list, key=len)
-        print(l2[-1])
+        # most recent experiment which was done by yolo modle processing
         exp = l2[-1]
 
         path = "yolov5-master/runs/detect/" + exp + "/labels/image.txt"
